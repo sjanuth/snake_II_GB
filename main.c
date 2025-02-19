e001f6dda20fa3b8855ee87b0da23321b11bf9f6
Silvan Januth <silvan.januth@wysszurich.ch>
2025-02-19 22:16:48 +0100

    added splash screen

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <gb/gb.h>
 #include <stdint.h>
+#include "LaroldsJubilantJunkyard.h"
+#include "splash_bg_asset.h"
 
 
 void main(void)
 {
+  DISPLAY_ON;
+  SHOW_BKG;
+
+     // Load & set our background data
+    set_bkg_data(0,splash_bg_asset_TILE_COUNT,splash_bg_asset_tiles);
+    
+    // The gameboy screen is 160px wide by 144px tall
+    // We deal with tiles that are 8px wide and 8px tall
+    // 160/8 = 20 and 120/8=15
+    set_bkg_tiles(0,1,20,15,splash_bg_asset_map);
+
     // Loop forever
     while(1) {
 
